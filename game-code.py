31bbb06c351b196c0f9b2a31963984d0b5715515
Dhruv Patil <138751440+Dhruv-Patil-1720@users.noreply.github.com>
2023-12-30 10:30:02 +0530

    Update game-code.py

--- game-code.py
+++ game-code.py
@@ -1,9 +1,6 @@
 import random
-
-# Define Possible Choices
 user_choices = ["rock", "paper", "scissors"]
 
-# Display Welcome Message
 print("Welcome to Rock, Paper, Scissors Game!")
 print("Rules: Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.")
 
@@ -11,28 +8,28 @@ print("Rules: Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.")
 player_score = 0
 computer_score = 0
 
-# Repeat Until Player Decides to Exit
+# Repeat Until the Player Decides to Exit
 while True:
-    # Display Menu Options
+    #  Menu Options
     print("\nMenu Options:")
     print("1. Rock")
     print("2. Paper")
     print("3. Scissors")
     print("4. Exit")
 
-    # Obtain User Input
+    #  User Input
     user_input = input("Enter your choice (1-4): ").lower()
 
     # If the Player chooses to Exit
     if user_input == "4":
         print("\nFinal Scores:")
         print(f"Player: {player_score}  Computer: {computer_score}")
-        print("Thanks for playing. Goodbye!")
+        print("Thank You for playing. Goodbye!")
         break
 
     # If the Player Chose Rock, Paper, or Scissors
     elif user_input in ["1", "2", "3"]:
-        # Generate Computer's Choice
+        #  Computer's Choice
         computer_choice = random.choice(user_choices)
 
         # Determine the Winner
@@ -43,16 +40,16 @@ while True:
             or (user_choices[int(user_input) - 1] == "paper" and computer_choice == "rock")
             or (user_choices[int(user_input) - 1] == "scissors" and computer_choice == "paper")
         ):
-            result = "You Win!"
+            result = "You Win!!"
             player_score += 1
         else:
-            result = "Computer Wins!"
+            result = "Computer Wins!!!!"
             computer_score += 1
 
-        # Display Results
+        # Results
         print(f"\nYour choice: {user_choices[int(user_input) - 1]}")
         print(f"Computer's choice: {computer_choice}")
         print(f"Result: {result}")
 
-    # Display Current Scores
+    # Current Scores
     print(f"\nCurrent Scores - Player: {player_score}  Computer: {computer_score}")
