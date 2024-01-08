d275d8fced75587544102d4353419719089dce1c
Dhruv Patil <138751440+Dhruv-Patil-1720@users.noreply.github.com>
2024-01-08 09:26:57 +0530

    Update game-code.py

--- game-code.py
+++ game-code.py
@@ -1,12 +1,12 @@
 import random
-user_choices = ["rock", "paper", "scissors"]
+choices = ["rock", "paper", "scissors"]
 
 print("Welcome to Rock, Paper, Scissors Game !!!!")
 print("Rules:  Remember Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.")
 
 # Initialize Scores
-player_score = 0
-computer_score = 0
+player-score = 0
+computer-score = 0
 
 # Repeat Until the Player Decides to Exit
 while True:
@@ -23,33 +23,33 @@ while True:
     # If the Player chooses to Exit
     if user_input == "4":
         print("\n Final Scores: " )
-        print(f" Player: {player_score}  Computer: {computer_score} ")
+        print(f" Player: {player-score}  Computer: {computer-score} ")
         print(" Thank You for playing. Goodbye!! ")
         break
 
     # If the Player Chose Rock, Paper, or Scissors
     elif user_input in ["1", "2", "3"]:
         #  Computer's Choice
-        computer_choice = random.choice(user_choices)
+        computer_choice = random.choice(choices)
 
         # Determine the Winner
-        if user_choices[int(user_input) - 1] == computer_choice:
+        if choices[int(user_input) - 1] == computer_choice:
             result = "It's a Tie!"
         elif (
-            (user_choices[int(user_input) - 1] == "rock" and computer_choice == "scissors")
-            or (user_choices[int(user_input) - 1] == "paper" and computer_choice == "rock")
-            or (user_choices[int(user_input) - 1] == "scissors" and computer_choice == "paper")
+            (choices[int(user_input) - 1] == "rock" and computer_choice == "scissors")
+            or (choices[int(user_input) - 1] == "paper" and computer_choice == "rock")
+            or (choices[int(user_input) - 1] == "scissors" and computer_choice == "paper")
         ):
             result = "You Win!!"
-            player_score += 1
+            player-score += 1
         else:
             result = "Computer Wins!!!!"
-            computer_score += 1
+            computer-score += 1
 
         # Results
-        print(f"\n Your choice: {user_choices[int(user_input) - 1]} ")
+        print(f"\n Your choice: {choices[int(user_input) - 1]} ")
         print(f" Computer's choice: {computer_choice} ")
         print(f" Result: {result} ")
 
     # Current Scores
-    print(f"\nCurrent Scores - Player: {player_score}  Computer: {computer_score}")
+    print(f"\nCurrent Scores - Player: {player-score}  Computer: {computer-score}")
